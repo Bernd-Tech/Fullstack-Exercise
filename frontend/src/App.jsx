df1e7356f363bfc90f88273a311df8be99af96c5
Bernd Pierre Louis <berndpierrelouis@icloud.com>
2025-09-10 23:34:46 +0200

    adding frontend setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Button } from "./components/navBar";
+
+const App = () => {
+
+  return (
+    <>
+      <Button text="Sign up"/>
+    </>
+  )
+}
+
+export default App
---
+++ frontend/src/components/navBar.jsx
@@ -0,0 +1,7 @@
+export const Button = ({text}) => {
+  return (
+    <>
+      <button className="bg-red-500 p-5">{text}</button>
+    </>
+  );
+};
