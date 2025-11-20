b3cf61f240510e29c8816443a21f8703c52a0e32
Bernd Pierre Louis <berndpierrelouis@icloud.com>
2025-11-20 23:59:14 +0100

    Finished supabase table set up and cleaned up code

--- frontend/src/pages/CompleteProfilePage.jsx
+++ frontend/src/pages/CompleteProfilePage.jsx
@@ -101,50 +101,9 @@ export const CompleteProfilePage = () => {
             <ReviewPage getValues={getValues}/>
         )}  
           </form>
-          <Button text="Sign Out" type="button" onClick={signOut} />
+          {/* <Button text="Sign Out" type="button" onClick={signOut} /> */}
         </div>
         </>)}
-        {/* <h1 className="text-5xl">Welcome to Essentia AI, {getValues("preferredName")}.</h1>
-        <p>To assure you have an enriching experience with Essentia, we kindly ask of you to fill out the following form as accurate as possible. This will help Essentia in assessing your needs pre-appointment and make the most of your valuable time.<br /> All information given will be kept confidential.</p>
-        <div className="flex gap-4">
-            <Button text="Consent Page" type="button" onClick={() => setCurrentPage(1)} />
-            <Button text="Personal Details" type="button" onClick={() => setCurrentPage(2)} />
-            <Button text="Initial Assessment" type="button" onClick={() => setCurrentPage(3)} />
-            <Button text="Summary Page" type="button" onClick={() => setCurrentPage(4)} />
-        </div>
-        <div className="bg-(--color-dark) w-[50%] rounded-[40px] p-8">
-          <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-8">
-            {currentPage === 1 && (
-              <>
-                <ConsentPage register={register} errors={errors} getValues={getValues}/>
-              </>
-            )}
-
-            {currentPage === 2 && (
-              <>
-                <CreateAccountPage register={register} errors={errors} getValues={getValues} watch={watch}/>
-              </>
-            )}
-
-            {currentPage === 3 && (
-              <>
-                <InitialAssessmentPage register={register} getValues={getValues} watch={watch}/>
-              </>
-            )}
-
-        {(currentPage < 4) && (
-            <div className="flex justify-between">
-            <Button type="button" onClick={prevPage} text="Back" />
-            <Button type="button" onClick={nextPage} text="Next" />
-            </div>
-        )}
-
-        {currentPage === 4 && ( 
-            <ReviewPage getValues={getValues}/>
-        )}  
-          </form>
-          <Button text="Sign Out" type="button" onClick={signOut} />
-        </div> */}
       </div>
     </>
   );
